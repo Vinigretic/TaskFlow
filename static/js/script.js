6ba3ed024d3dd1b6db3b7efbcedcf2eedd5400e8
Bakatina Vita <v.bakatina@gmail.com>
2024-03-19 15:46:29 +0200

    feat: add HTMX to project - connect HTMX to base.html - create template for checkbox - update class TaskUpdateCheckBoxView - update template project_list_with_tasks.html - remove method from script.js to check checkbox

--- static/js/script.js
+++ static/js/script.js
@@ -18,24 +18,6 @@
   });
 });
 
-/**
- * Attaches an event listener to each found checkbox element.
- * When the checkbox is changed, it runs a function to find the form element,
- * then submits the form.
- */
 
-// Find all the checkboxes on the page
-var checkboxes = document.querySelectorAll('.form-check-input');
-
-// For each checkbox add an event handler
-checkboxes.forEach(function(checkbox) {
-    checkbox.addEventListener('change', function() {
-        // Finding form
-        var form = checkbox.closest('form');
-
-        // Send the form when the checkbox is changed
-        form.submit();
-    });
-});
 
 
